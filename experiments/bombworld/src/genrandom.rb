d94e6af182892a0b05e2df4dd4d7e2d5b36dead0
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2010-01-18 22:40:57 +0000

    Changed ruby script to average a number of tests

--- experiments/bombworld/src/genrandom.rb
+++ experiments/bombworld/src/genrandom.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby
 
 for i in 1..10 do
-  file = File.open("stats"+i+".txt","r")
+  file = File.open("stats"+i.to_s()+".txt","r")
   total=0
   divisor=0
   while(line = file.gets)
@@ -11,11 +11,11 @@ for i in 1..10 do
   average = total/divisor
   file.close
   file = File.open("stats.txt","a")
-  file.puts(""+i.to_i+" "+average+"\n")
+  file.puts(""+i.to_s()+" "+average.to_s()+"\n")
   file.close
   
   #Nasty non parameterized code repeat, I know
-  file = File.open("statsNormative"+i+".txt","r")
+  file = File.open("statsNormative"+i.to_s()+".txt","r")
   total=0
   divisor=0
   while(line = file.gets)
@@ -25,6 +25,6 @@ for i in 1..10 do
   average = total/divisor
   file.close
   file = File.open("statsNormative.txt","a")
-  file.puts(""+i.to_i+" "+average+"\n")
+  file.puts(""+i.to_s()+" "+average.to_s()+"\n")
   file.close
 end
